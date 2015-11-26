e550b421173134ac17024bca4a0a14a34b330894
mngaw20 <mngaw20@gmail.com>
2015-11-26 16:41:39 -0500

    init commit

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+	twitter: {
+  	consumer_key: '7FfFtReZ3YrDoXoevKOHgrjRC',
+		consumer_secret: '5g3eKoUCiUh6P7FqJrWAhiIttdLrWQNaE0l4jPfDfrJXUjhrE7',
+		access_token: '4267034835-Hzpoyl1beDvqV10RKQ7UmCirscBKBL0WJLxCk8i',
+		access_token_secret: 'WuXpMHHuGp5CRvmYgi3ZmafitiXHZxpCHRl7U0XxaoNpE'
+	}
+
+};
